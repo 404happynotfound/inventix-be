10e83ef4e0369b7e378e746f53789319c49110aa
404happynotfound <happycalista404@gmail.com>
2026-05-11 23:16:11 +0700

    feat: add akun routes

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,13 @@ import swaggerUi from 'swagger-ui-express';
 import { generateOpenApiDocument } from './config/openapi';
 
 import authRoutes from './modules/auth/auth.route';
-import userRoutes from './modules/user/user.route';
+import akunRoutes from './modules/akun/akun.route';
+// import supplierRoutes from './modules/supplier/supplier.route';
+// import stokRoutes from './modules/stok/stok.route';
+// import klasifikasiStokRoutes from './modules/klasifikasi-stok/klasifikasi-stok.route';
+// import purchaseOrderRoutes from './modules/purchase-order/purchase-order.route';
+// import pembelianTransaksiRoutes from './modules/pembelian-transaksi/pembelian-transaksi.route';
+// import riwayatAktivitasRoutes from './modules/riwayat-aktivitas/riwayat-aktivitas.route';
 import { errorHandler } from './middlewares/errorHandler';
 import { httpLogger } from './middlewares/logger';
 
@@ -19,7 +25,13 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
-app.use('/api/user', userRoutes);
+app.use('/api/akun', akunRoutes);
+// app.use('/api/supplier', supplierRoutes);
+// app.use('/api/stok', stokRoutes);
+// app.use('/api/klasifikasi-stok', klasifikasiStokRoutes);
+// app.use('/api/purchase-order', purchaseOrderRoutes);
+// app.use('/api/pembelian-transaksi', pembelianTransaksiRoutes);
+// app.use('/api/riwayat-aktivitas', riwayatAktivitasRoutes);
 
 // OpenAPI Documentation Route
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(generateOpenApiDocument()));
